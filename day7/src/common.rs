f08d09e603ce0e05c62c0840f04a0d381e444c00
Robin <robin.robbe96@gmail.com>
2025-12-07 12:50:54 +0100

    Visually optimized day 7

--- day7/src/common.rs
+++ day7/src/common.rs
@@ -1,20 +1,11 @@
 use anyhow::{Result, bail};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Location {
     Start,
     Empty,
     Splitter,
     TachyonBeam(u64),
-}
-
-impl PartialEq for Location {
-    fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            (Self::TachyonBeam(_), Self::TachyonBeam(_)) => true, // Ignore the number inside the beam when comparing
-            _ => core::mem::discriminant(self) == core::mem::discriminant(other),
-        }
-    }
 }
 
 impl Location {
@@ -44,16 +35,22 @@ impl Manifold {
 
     fn are_all_same_kind(
         diagram: &Vec<Vec<Location>>,
-        kind: &Location,
-        locations: Vec<(usize, usize)>,
+        location_kind: &Location,
+        all_coordinates: Vec<(usize, usize)>,
     ) -> bool {
-        locations.into_iter().all(|locations| {
-            let y = locations.0;
-            let x = locations.1;
-            let c = diagram.get(y).unwrap().get(x).unwrap();
+        all_coordinates
+            .into_iter()
+            .all(|coordinates: (usize, usize)| {
+                let y = coordinates.0;
+                let x = coordinates.1;
+                let c = &diagram[y][x];
 
-            c == kind
-        })
+                // Ignores the number inside the TachyonBeam
+                matches!(
+                    (c, location_kind),
+                    (&Location::TachyonBeam(_), &Location::TachyonBeam(_))
+                )
+            })
     }
 
     fn find_splitters_with_beams(&self) -> u64 {
@@ -64,11 +61,15 @@ impl Manifold {
 
         for y in 0..height {
             for x in 0..width {
-                let c = self.diagram.get(y).unwrap().get(x).unwrap();
+                let c = &self.diagram[y][x];
                 // Check if c is splitter and it has 1 beam above and 2 to the sides it
                 if c == &Location::Splitter
                     && Self::are_all_same_kind(
                         &self.diagram,
+                        // It feels a bit silly to create a new Beam here when
+                        // we just want to compare the type. I have looked at
+                        // std::mem::discriminant, but it still does not fully
+                        // avoid this...
                         &Location::TachyonBeam(0),
                         vec![(y - 1, x), (y, x - 1), (y, x + 1)],
                     )
@@ -81,7 +82,8 @@ impl Manifold {
         count
     }
 
-    fn fine_timelines(&self) -> u64 {
+    fn find_timelines(&self) -> u64 {
+        // Simply count the numbers inside the tachyon beams on the last line
         self.diagram
             .last()
             .unwrap()
@@ -93,97 +95,58 @@ impl Manifold {
             .sum()
     }
 
+    fn check_left_right(&mut self, (y, x): (usize, usize), beam_count: u64) {
+        let c_left_right = match self.diagram[y].get_mut(x) {
+            Some(c_left_right) => c_left_right,
+            None => return,
+        };
+
+        match *c_left_right {
+            Location::TachyonBeam(i) => *c_left_right = Location::TachyonBeam(i + beam_count),
+            Location::Empty => *c_left_right = Location::TachyonBeam(beam_count),
+            Location::Splitter => panic!("Splitter next to splitter!"),
+            _ => {
+                panic!("Did not expect {c_left_right:?} at y: {y}, x: {x}",)
+            }
+        }
+    }
+
     pub fn tachyon_beam_split_count(&mut self, timelines: bool) -> u64 {
         let height = self.diagram.len();
         let width = self.diagram.first().unwrap().len();
 
         for y in 0..height {
             for x in 0..width {
-                let c = self.diagram.get(y).unwrap().get(x).unwrap();
-                if c == &Location::TachyonBeam(0) || c == &Location::Start {
-                    // TODO: Can do this in a better way?
-                    let beam_count = match *c {
-                        Location::TachyonBeam(i) => i,
-                        Location::Start => 1,
-                        _ => panic!(),
-                    };
-                    // Send beam downwards
-                    if y + 1 == height {
-                        break;
-                    }
+                let c = &self.diagram[y][x];
+                let beam_count = match *c {
+                    Location::TachyonBeam(i) => i,
+                    Location::Start => 1,
+                    _ => continue,
+                };
 
-                    let c_below = self.diagram.get_mut(y + 1).unwrap().get_mut(x).unwrap();
-                    match *c_below {
-                        Location::TachyonBeam(i) => {
-                            // Merge tachyon beams
-                            *c_below = Location::TachyonBeam(i + beam_count)
-                        }
-                        Location::Empty => *c_below = Location::TachyonBeam(beam_count),
-                        Location::Splitter => {
-                            // TODO: Would be nice to break out this code
-                            if let Some(c_below_left) =
-                                self.diagram.get_mut(y + 1).unwrap().get_mut(x - 1)
-                            {
-                                match *c_below_left {
-                                    Location::TachyonBeam(i) => {
-                                        *c_below_left = Location::TachyonBeam(i + beam_count)
-                                    }
-                                    Location::Empty => {
-                                        *c_below_left = Location::TachyonBeam(beam_count)
-                                    }
-                                    Location::Splitter => panic!("Splitter next to splitter!"),
-                                    _ => {
-                                        panic!(
-                                            "Did not expect {c_below_left:?} at y: {}, x: {}",
-                                            y + 1,
-                                            x - 1
-                                        )
-                                    }
-                                }
-                            }
-                            if let Some(c_below_right) =
-                                self.diagram.get_mut(y + 1).unwrap().get_mut(x + 1)
-                            {
-                                match *c_below_right {
-                                    Location::TachyonBeam(i) => {
-                                        *c_below_right = Location::TachyonBeam(i + beam_count)
-                                    }
-                                    Location::Empty => {
-                                        *c_below_right = Location::TachyonBeam(beam_count)
-                                    }
-                                    Location::Splitter => panic!("Splitter next to splitter!"),
-                                    _ => {
-                                        panic!(
-                                            "Did not expect {c_below_right:?} at y: {}, x: {}",
-                                            y + 1,
-                                            x + 1
-                                        )
-                                    }
-                                }
-                            }
-                        }
-                        _ => panic!("Did not expect {c_below:?} at y: {}, {x}: x", y + 1),
+                // Check if we have reached the bottom
+                if y + 1 == height {
+                    break;
+                }
+
+                // Send the beam downwards
+                let c_below = &mut self.diagram[y + 1][x];
+                match *c_below {
+                    Location::TachyonBeam(i) => *c_below = Location::TachyonBeam(i + beam_count),
+                    Location::Empty => *c_below = Location::TachyonBeam(beam_count),
+                    Location::Splitter => {
+                        self.check_left_right((y + 1, x - 1), beam_count);
+                        self.check_left_right((y + 1, x + 1), beam_count);
                     }
+                    _ => panic!("Did not expect {c_below:?} at y: {}, {x}: x", y + 1),
                 }
             }
         }
 
         if timelines {
-            self.fine_timelines()
+            self.find_timelines()
         } else {
             self.find_splitters_with_beams()
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_location_partial_eq() {
-        assert_eq!(&Location::Empty, &Location::Empty);
-        assert_eq!(&Location::TachyonBeam(5), &Location::TachyonBeam(1));
-        assert_ne!(&Location::Empty, &Location::TachyonBeam(1));
     }
 }
